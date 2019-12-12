516cff46fda2a6e84af0b27c220cecb8be3f3352
tarun <gargtarun08@gmail.com>
2019-12-12 19:41:42 +0530

    labeled_groceries added

--- label_knn.py
+++ label_knn.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 file_name = 'ingredients.csv'
@@ -49,4 +50,42 @@ print("Accuracy is ", accuracy_score(y_test, y_pred) * 100, "% for K-Value:", 16
 label_pred = neigh.predict(X_label)
 label_pred_prob = neigh.predict_proba(X_label)
 # use label_pred_prob to find out two most probable cuisine and write them in groceries.csv file
+# Index for Cuisines
+ind=['brazilian','british','cajun_creole','chinese','filipino','french','greek','indian','italian','jamaican','japanese','korean','mexican','moroccan','russian','southern_us','spanish','thai','vietnamese','irish','none']
+probability_array=[]
+
+# finding two best probabilities
+for i in range(len(label_pred_prob)):
+    tmp_list=list(label_pred_prob[i])
+    x=tmp_list.index(max(tmp_list))
+    ma=max(tmp_list)
+    y=0
+    for j in range(len(tmp_list)):
+        if(y<tmp_list[j] and ma>tmp_list[j]):
+            y=tmp_list[j]
+    if(y>0.1):
+        y=tmp_list.index(y)
+    else:
+        y=20
+    # storing two probabilities in a list
+    probability_array.append(ind[x])
+    probability_array.append(ind[y])
+
+file_name = 'groceries.csv'
+
+# writing cuisines names to CSV
+with open(file_name,'r') as csvinput:
+    with open('labeled_groceries.csv', 'w',newline='') as csvoutput:
+        writer = csv.writer(csvoutput)
+        count=0
+        i=0
+        for row in csv.reader(csvinput):
+            if(count==0):
+                writer.writerow(['Cuisine 1', "Cuisine 2"] + row)
+            else:
+                writer.writerow([probability_array[i],probability_array[i+1]]+row)
+                i=i+2
+            count+=1
+            #print(probability_array[i],probability_array[i+1])
+
 
